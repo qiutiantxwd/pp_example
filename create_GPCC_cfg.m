98bd6b98c77184333a59116deb0de73ae71c6282
Tian Qiu <tiq014@eng.ucsd.edu>
2022-05-17 16:14:06 -0700

    Update test data cfg file

--- create_GPCC_cfg.m
+++ create_GPCC_cfg.m
@@ -1,4 +1,5 @@
 % Create cfg file for WPI test data.
 clear; close all;clc
 % load one PCC for visualization.
-base_dir='/home/tianqiu/Documents/Research/PointPillars_example_20b';
+base_dir='/home/tianqiu/Documents/Research/PointPillars_example_20b/WPI_LidarData/TestData';
+
